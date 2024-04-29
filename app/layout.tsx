502bc61666427ac31518f033d526970c874a67d3
lakindu62 <lakindu62@gmail.com>
2024-04-29 14:04:39 +0530

    tailwind lecture

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,18 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font";
+import { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "A community-driven platform for asking and answering programming questions.Get help, share knowledge, and collaborate with developers from around the world.Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -8,7 +21,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <h1 className="h1-bold">This is a Piece of text</h1>
+
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
